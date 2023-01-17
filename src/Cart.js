524d186aa300f3d8532c817700ab4fe4e7034852
Guilherme-BS <guilherme.bbsa@gmail.com>
2023-01-17 11:01:53 -0300

    initial unit test studies

---
+++ src/Cart.js
@@ -0,0 +1,47 @@
+import find from 'lodash/find'
+import remove from 'lodash/remove'
+
+
+export default class Cart {
+  items = [];
+
+  getTotal() {
+    return this.items.reduce((acc, item) => {
+      return acc + item.quantity * item.product.price
+    }, 0);
+  }
+
+  add(item) {
+    const itemToFind = { product: item.product }
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind)
+    }
+    this.items.push(item)
+  }
+
+  remove(product) {
+    remove(this.items, { product })
+  }
+
+  summary() {
+    const total = this.getTotal();
+    const items = this.items;
+
+
+    return {
+      total,
+      items,
+    }
+  }
+
+  checkout() {
+    const {total, items} = this.summary();
+
+    this.items = []
+
+    return {
+      total,
+      items,
+    }
+  }
+}
